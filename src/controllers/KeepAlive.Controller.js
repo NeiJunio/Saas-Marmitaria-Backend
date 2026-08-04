e708fcc26be517676c5329f1575553837ee56967
NeiJunio <neijunio50@gmail.com>
2026-08-03 23:13:05 -0300

    Update KeepAlive.Controller.js

--- src/controllers/KeepAlive.Controller.js
+++ src/controllers/KeepAlive.Controller.js
@@ -1,7 +1,21 @@
 // src/controllers/keepAliveController.js
 
+// export const ping = (req, res) => {
+//     // Retorna apenas um 200 OK muito leve
+//     return res.status(200).json({
+//         status: 'online',
+//         message: 'A API está acordada e operante!',
+//         timestamp: new Date().toISOString()
+//     });
+// };
+
+
 export const ping = (req, res) => {
-    // Retorna apenas um 200 OK muito leve
+    const agora = new Date().toLocaleString('pt-BR', { timeZone: 'America/Sao_Paulo' });
+    
+    // 👇 Esse é o print que vai aparecer lá no painel da Render!
+    console.log(`[${agora}] 🤖 Bip Bop! Ping recebido do cron-job.org. API acordada!`);
+
     return res.status(200).json({
         status: 'online',
         message: 'A API está acordada e operante!',
